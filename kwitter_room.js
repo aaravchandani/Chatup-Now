bb859369d801623e9bec027138fcabcbeb7a1a35
aaravchandani <81931219+aaravchandani@users.noreply.github.com>
2021-08-22 17:44:54 -0700

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -21,3 +21,6 @@ function getData() {firebase.database().ref("/").on('value', function(snapshot) 
       //End code
       });});}
 getData();
+function logout(){
+  window.location = "index.html"
+}
